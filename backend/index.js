92d26b6a12d4f9db7db6c9ac60bd45f52f3cf85d
Promi-15 <promii1217@gmail.com>
2025-05-28 22:42:12 +0600

    crud aomplete backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./config/db.js";
 import Coffee from "./models/coffeeModel.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -18,11 +19,58 @@ app.post("/api/coffee",async (req, res) => {
     const newCoffee = new Coffee(coffee)
     try {
         await newCoffee.save()
-        res.status(200).json({message : "product added successfully"})
+        res.status(200).json({message : "product added successfully",data : newCoffee})
     } catch (error) {
         res.status(500).json({message : "server error"})
     }
 });
+
+
+app.get("/api/coffee", async (req, res) => {
+    try {
+        const coffees = await Coffee.find({})
+        res.status(200).json({data : coffees})
+    } catch (error) {
+         res.status(500).json({message : "server error"})
+    }
+})
+
+app.delete("/api/coffee/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        const coffee = await Coffee.findById(id)
+        if (!coffee) {
+            res.status(400).json({message : "not found"})
+        }
+        await Coffee.deleteOne({_id : id})
+         res.status(200).json({ success: true, message: "product deleted" });
+    } catch (error) {
+          res.status(500).json({message : "server error"})
+    }
+})
+
+app.put("/api/coffee/:id", async(req, res) => {
+   
+  const { id } = req.params;
+  const coffee = req.body;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "Invalid id" });
+  }
+
+  try {
+    const updatedCoffee = await Coffee.findByIdAndUpdate(id, coffee, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedCoffee });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "server error" });
+  }
+
+
+})
+
+
+
 
 app.listen(PORT, () => {
   connectDb();
