8c18a5065bfbad1368f77fb71187db7d23e31c36
Promi-15 <promii1217@gmail.com>
2025-05-28 20:57:37 +0600

    list component

--- frontend/src/Pages/HomePage.jsx
+++ frontend/src/Pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../Components/Banner";
+import Lists from "../Components/Lists";
 
 const HomePage = () => {
   return (
     <div className="">
       <Banner />
+      <Lists/>
     </div>
   );
 };
