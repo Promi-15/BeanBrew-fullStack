0190291a1df1d34ad76cc4f070af7748bc512f6e
Promi-15 <promii1217@gmail.com>
2025-05-28 22:21:43 +0600

    create product in backend

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,30 @@
-import express from "express"
-import dotenv from "dotenv"
-import { connectDb } from "./config/db.js"
+import express from "express";
+import dotenv from "dotenv";
+import { connectDb } from "./config/db.js";
+import Coffee from "./models/coffeeModel.js";
 
-dotenv.config()
-const PORT = process.env.PORT
+dotenv.config();
+const PORT = process.env.PORT;
 
-const app = express()
+const app = express();
+app.use(express.json())
 
+app.post("/api/coffee",async (req, res) => {
+    const coffee = req.body;
+    if (!coffee.name || !coffee.price || !coffee.image) {
+        return res.status(400).json({message : "please provide all fields"})
+    }
 
+    const newCoffee = new Coffee(coffee)
+    try {
+        await newCoffee.save()
+        res.status(200).json({message : "product added successfully"})
+    } catch (error) {
+        res.status(500).json({message : "server error"})
+    }
+});
 
-app.get("/coffeeLists", (req, res) => {
-    
-})
 app.listen(PORT, () => {
-   connectDb()
-    console.log(`server is running at port ${PORT}`)
-})
-
+  connectDb();
+  console.log(`server is running at port ${PORT}`);
+});
---
+++ backend/models/coffeeModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const coffeeSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Coffee = mongoose.model("Coffee", coffeeSchema);
+export default Coffee;
