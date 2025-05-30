7dfe654e68a5fbc87a72a49f31a86b83bf7258cc
Promi-15 <promii1217@gmail.com>
2025-05-30 20:56:34 +0600

    menu design

--- frontend/src/Components/Banner.jsx
+++ frontend/src/Components/Banner.jsx
@@ -11,18 +11,18 @@ const Banner = () => {
     >
       <div className=" opacity-50 absolute bg-black inset-0  top-20 "></div>
 
-      <div className="top-80 absolute p-10 space-y-3.5  lg:w-[900px]" >
+      <div className="top-72 absolute p-10 space-y-10  lg:w-[900px]" >
         <p className="text-white font-medium text-md italic">crafted with love, served with passion</p>
-        <h1 className="text-white text-4xl lg:text-7xl font-semibold"   style={{ fontFamily: "'Prata', serif" }}>
+        <h1 className="text-white text-4xl lg:text-7xl font-semibold italic"   style={{ fontFamily: "'Prata', serif" }}>
          Step into the aroma of <br /> freshly
           coffee
         </h1>
-        <p className="text-white" >
+        <p className="text-white opacity-35" >
           Discover a place where every cup is a masterpiece, crafted with
           passion and precision. From the rich, bold flavors of our signature
           blends to the cozy ambiance that feels like home.
         </p>
-        <button className="btn">Discover Coffee</button>
+        <button className="btn bg-gradient-to-l  from-bg-red-950 to-red-500 p-5 font-bold hover:shadow-2xl hover:shadow-white">Discover Coffee</button>
       </div>
     </div>
   );
--- frontend/src/Components/Menu.jsx
+++ frontend/src/Components/Menu.jsx
@@ -1,40 +1,143 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 const Menu = () => {
   return (
-    <div className="my-10 text-center">
-      <p className="text-center text-5xl font-bold italic">Our Menu</p>
-      <div className="grid lg:grid-cols-3  gap-5 mt-10 ml-15 grid-cols-1">
-        <div>
-          <img
-            src="/coldCof1.jpg"
-            className="rounded-xl hover:shadow-2xl lg:h-[480px]"
-            alt=""
-          />
-          <p className="text-center font-semibold italic mt-5 text-2xl">
-            Cold coffee
-          </p>
+    <div className="py-14 text-center bg-black">
+      <p className="text-center text-5xl font-bold italic text-white">
+        Our Menu
+      </p>
+      <p className="text-center italic text-sm opacity-40 max-w-7xl mx-auto text-white">
+        A coffee menu offers a delightful variety of beverages made from rich,
+        roasted coffee beans. It typically includes classics like espresso,
+        cappuccino, latte, and Americano, along with iced options and flavored
+        drinks such as caramel macchiato or mocha. Whether you prefer something
+        strong and bold or smooth and creamy, the coffee menu caters to all
+        tastes, making it perfect for every coffee lover.
+      </p>
+      <div className="grid lg:grid-cols-2 grid-cols-1">
+        <div className="lg:w-[650px] mx-auto my-10 bg-gray-200 p-5  space-y-6 ">
+          <div className="flex flex-row justify-between">
+            <img
+              src="../../public/expresso.jpg"
+              className="w-15 rounded-full"
+              alt=""
+            />
+            <div className="flex flex-col">
+              <p className="font-bold italic underline">Americano Espresso</p>
+              <p className="font-light ">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea,
+                maxime.
+              </p>
+            </div>
+            <div className=" h-15 border-1 border-black"> </div>
+            <div>
+              <p className="font-medium">$183.3</p>
+              <p className="opacity-35">
+                {" "}
+                <s>$200</s>
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row justify-between gap-5">
+            <img
+              src="../../public/coldCof1.jpg"
+              className="w-15 rounded-full"
+              alt=""
+            />
+            <div className="flex flex-col">
+              <p className="font-bold italic underline">Mericano Cold Coffee</p>
+              <p className="font-light ">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea,
+                maxime.
+              </p>
+            </div>
+            <div className=" h-15 border-1 border-black"> </div>
+            <div>
+              <p className="font-medium">$183.3</p>
+              <p className="opacity-35">
+                {" "}
+                <s>$200</s>
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row justify-between gap-5">
+            <img
+              src="../../public/iceCofi1.jpg"
+              className="w-15 rounded-full"
+              alt=""
+            />
+            <div className="flex flex-col">
+              <p className="font-bold italic underline">
+                Americano Roasted Ice Latte
+              </p>
+              <p className="font-light ">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea,
+                maxime.
+              </p>
+            </div>
+            <div className=" h-15 border-1 border-black"> </div>
+            <div>
+              <p className="font-medium">$183.3</p>
+              <p className="opacity-35">
+                {" "}
+                <s>$200</s>
+              </p>
+            </div>
+          </div>
+          <div className="flex flex-row justify-between gap-5">
+            <img
+              src="../../public/matcha1.jpg"
+              className="w-15 rounded-full"
+              alt=""
+            />
+            <div className="flex flex-col">
+              <p className="font-bold italic underline">Matcha</p>
+              <p className="font-light ">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea,
+                maxime.
+              </p>
+            </div>
+            <div className=" h-15 border-1 border-black"> </div>
+            <div>
+              <p className="font-medium">$103.3</p>
+              <p className="opacity-35">
+                {" "}
+                <s>$100</s>
+              </p>
+            </div>
+          </div>
         </div>
-        <div>
-          <img
-            src="/expresso.jpg"
-            className="rounded-xl hover:shadow-2xl lg:h-[480px]"
-            alt=""
-          />
-          <p className="text-center font-semibold italic mt-5 text-2xl">
-            Espresso
-          </p>
-        </div>
-        <div>
-          <img
-            src="/matcha1.jpg"
-            className="rounded-xl hover:shadow-2xl lg:h-[480px]"
-            alt=""
-          />
-          <p className="text-center font-semibold italic mt-5 text-2xl">
-            Matcha
-          </p>
+        <div className="lg:w-[650px] my-10 ">
+          <p className="italic font-bold bg-gradient-to-bl from-red-950 to-pink-900 p-6"> Time to open and close</p>
+          <div className="flex flex-row justify-between  bg-gray-200 px-10 py-3.5">
+            <p>Sunday</p>
+            <p>Closed</p>
+          </div>
+          <hr />
+          <div className="flex flex-row justify-between  bg-gray-200 px-10  py-3.5">
+            <p>Monday</p>
+            <p>6.00am-7.00pm</p>
+          </div>
+          <hr />
+          <div className="flex flex-row justify-between  bg-gray-200 px-10  py-3.5">
+            <p>Tuesday</p>
+            <p>6.00am-7.00pm</p>
+          </div>
+          <hr />
+          <div className="flex flex-row justify-between  bg-gray-200 px-10  py-3.5">
+            <p>Wednesday</p>
+            <p>6.00am-7.00pm</p>
+          </div>
+          <hr />
+          <div className="flex flex-row justify-between  bg-gray-200 px-10  py-3.5">
+            <p>Thursday</p>
+            <p>6.00am-7.00pm</p>
+          </div>
+          <hr />
+          <div className="flex flex-row justify-between  bg-gray-200 px-10  py-3.5">
+            <p>Friday</p>
+            <p>6.00am-3.00pm</p>
+          </div>
         </div>
       </div>
       <Link to={"/lists"}>
