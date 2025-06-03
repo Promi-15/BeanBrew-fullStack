6e6e1aacddb15ce7683797f9c8fda2bbde4af11c
Promi-15 <promii1217@gmail.com>
2025-06-03 19:37:29 +0600

    vdo section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import Navbar from "./Components/Navbar";
--- frontend/src/Components/DiscoverBanner.jsx
+++ frontend/src/Components/DiscoverBanner.jsx
@@ -7,7 +7,21 @@ const DiscoverBanner = () => {
               <p>Discover our story</p>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro itaque, cupiditate et qui placeat nesciunt eos aspernatur maiores accusantium ipsa ipsum beatae unde deserunt sit fugit consectetur asperiores nihil enim nostrum nobis quo suscipit nemo repellendus id! Quaerat quisquam rerum atque? Dolor necessitatibus, est sunt nesciunt dicta dignissimos? Et, magnam.</p>
       </div>
-      <div></div>
+          <div>
+              <p>Discover our story</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro itaque, cupiditate et qui placeat nesciunt eos aspernatur maiores accusantium ipsa ipsum beatae unde deserunt sit fugit consectetur asperiores nihil enim nostrum nobis quo suscipit nemo repellendus id! Quaerat quisquam rerum atque? Dolor necessitatibus, est sunt nesciunt dicta dignissimos? Et, magnam.</p>
+      </div>
+          <div>
+              <p>Discover our story</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro itaque, cupiditate et qui placeat nesciunt eos aspernatur maiores accusantium ipsa ipsum beatae unde deserunt sit fugit consectetur asperiores nihil enim nostrum nobis quo suscipit nemo repellendus id! Quaerat quisquam rerum atque? Dolor necessitatibus, est sunt nesciunt dicta dignissimos? Et, magnam.</p>
+      </div>
+          <div>
+              <p>Discover our story</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro itaque, cupiditate et qui placeat nesciunt eos aspernatur maiores accusantium ipsa ipsum beatae unde deserunt sit fugit consectetur asperiores nihil enim nostrum nobis quo suscipit nemo repellendus id! Quaerat quisquam rerum atque? Dolor necessitatibus, est sunt nesciunt dicta dignissimos? Et, magnam.</p>
+      </div>
+          <div>
+              <video src=""></video>
+      </div>
     </div>
   )
 }
