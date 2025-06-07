87b4a92264ab6d6f335a926cdf0f9fe6f6496e8d
Promi-15 <promii1217@gmail.com>
2025-06-07 22:53:15 +0600

    login functionality with auth0

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,36 +1,95 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const { loginWithRedirect } = useAuth0();
+  const { user, isAuthenticated, logout } = useAuth0();
+
+  console.log(user);
   return (
     <div className="bg-gray-200  rounded-sm flex justify-between items-center px-10  top-0 w-full z-50 shadow-md relative">
       <div className="flex items-center">
-       
         <Link to={"/"}>
-       <img src="/logoN.png " className="w-24 h-20" alt="" />
+          <img src="/logoN.png " className="w-24 h-20" alt="" />
           <img
-          src="/logoimg.png"
-          className="w-20 pt-3 opacity-45 absolute left-28 top-0"
-          alt=""
-        /></Link>
+            src="/logoimg.png"
+            className={`w-20 pt-3 opacity-45 absolute left-28 top-0 `}
+            alt=""
+          />
+        </Link>
       </div>
       <div>
-        <img
-          src="/cat.jpg"
-          className="w-15 h-15 rounded-full cursor-pointer"
-          alt=""
-          onClick={() => setIsOpen(!isOpen)}
-        />
+        {isAuthenticated ? (
+          <div className=" relative group ">
+            <img
+              src={user.picture}
+              className={`w-12 h-12 rounded-full cursor-pointer `}
+              alt=""
+              onClick={() => setIsOpen(!isOpen)}
+            />
+            <p className="absolute top-10 text-sm  shadow-xl w-[100px] shadow-white  opacity-0  group-hover:opacity-100 transition duration-200  hover:cursor-pointer text-black mt-0.5 ">
+              {user.name}
+            </p>
+          </div>
+        ) : (
+          <div className=" relative group ">
+            <img
+              src="/cat.jpg"
+              className="w-15 h-15 rounded-full cursor-pointer"
+              alt=""
+              onClick={() => setIsOpen(!isOpen)}
+            />
+            <p className="absolute top-10 text-sm  shadow-xl w-[100px] shadow-white  opacity-0  group-hover:opacity-100 transition duration-200  hover:cursor-pointer text-black mt-0.5 right-1">
+              user
+            </p>
+          </div>
+        )}
         {isOpen && (
-          <div className="absolute bg-gray-200 top-24 right-5  rounded-xl   bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 border border-gray-100">
-            <ul
-              className="  space-y-1.5 text-sm  "
-            >
-
-           
-             <Link to={"/createNewCoffee"}> <li className="hover:bg-cyan-100 px-8 py-2">ADD New items</li></Link>
-              <Link to={"/login"}><li className="hover:bg-cyan-100 px-8 py-2">Login</li> </Link>
+          <div className="absolute bg-gray-200 top-24 right-5  rounded-xl   bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 border border-gray-100 text-center">
+            <ul className="  space-y-1.5 text-sm  ">
+              <Link to={"/createNewCoffee"}>
+                {" "}
+                <li className="hover:bg-cyan-100 px-8 py-2">ADD New items</li>
+              </Link>
+              {/* <Link to={"/login"}>
+                <li className="hover:bg-cyan-100 px-8 py-2">Login</li>{" "}
+              </Link> */}
+              {!isAuthenticated && (
+                <button
+                  onClick={() => loginWithRedirect()}
+                  className="hover:bg-cyan-100 w-full py-2"
+                >
+                  Log In
+                </button>
+              )}
+              {/* <button
+                onClick={() => loginWithRedirect()}
+                className="hover:bg-cyan-100  w-full py-2"
+              >
+                Log In
+              </button> */}
+              {isAuthenticated && (
+                <button
+                  onClick={() =>
+                    logout({
+                      logoutParams: { returnTo: window.location.origin },
+                    })
+                  }
+                  className="hover:bg-cyan-100 w-full py-2"
+                >
+                  Log Out
+                </button>
+              )}
+              {/* <button
+                className="hover:bg-cyan-100  w-full py-2"
+                onClick={() =>
+                  logout({ logoutParams: { returnTo: window.location.origin } })
+                }
+              >
+                Log Out
+              </button> */}
               <li className="hover:bg-cyan-100 px-8 py-2">Register</li>
             </ul>
           </div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import { Auth0Provider } from '@auth0/auth0-react';
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Auth0Provider
+    domain="dev-e8duogmyorgbl74g.us.auth0.com"
+    clientId="RJZH1vBMEHQ1L5nR9X3fJS55xgsP0yCw"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </Auth0Provider>
+  ,
 )
